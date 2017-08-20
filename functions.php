39f076315c8c61d4d50487b6b5c599fa5fc33f96
ENGAG3 Media <media@engag3.us>
2017-08-20 13:13:49 -0700

    Fixed Ig feeds and removed old scripts

--- functions.php
+++ functions.php
@@ -24,11 +24,11 @@
 
  	wp_enqueue_script( 'boa-script', get_stylesheet_directory_uri() . '/assets/js/script.js', array('jquery'), '20151215', true );
 
- 	wp_enqueue_script( 'bao-linkify', get_stylesheet_directory_uri() . '/assets/js/linkify.min.js', array(), '20151215', true );
+ // 	wp_enqueue_script( 'bao-linkify', get_stylesheet_directory_uri() . '/assets/js/linkify.min.js', array(), '20151215', true );
 
- 	wp_enqueue_script( 'bao-linkify_jQuery', get_stylesheet_directory_uri() . '/assets/js/linkify-jquery.min.js', array('jquery'), '20151215', true );
+ // 	wp_enqueue_script( 'bao-linkify_jQuery', get_stylesheet_directory_uri() . '/assets/js/linkify-jquery.min.js', array('jquery'), '20151215', true );
 
- 	wp_enqueue_script( 'bao-clipboard', get_stylesheet_directory_uri() . '/assets/js/clipboard.min.js', array('jquery'), '20151215', true );
+ // 	wp_enqueue_script( 'bao-clipboard', get_stylesheet_directory_uri() . '/assets/js/clipboard.min.js', array('jquery'), '20151215', true );
 
  }
  add_action( 'wp_enqueue_scripts', 'bao_scripts' );
